0c9bbd47edae5f46e9d48363ede01a7e5c745618
abdullaqudrat <aqudrat630@gmail.com>
2019-06-28 16:37:31 -0600

    Updates users show to reflect requirements
    requirements:
    -show year for date released
    -calculate number of years since release date

--- tests/Browser/ShowPageTest.php
+++ tests/Browser/ShowPageTest.php
@@ -18,19 +18,37 @@ class UserShowPageTest extends DuskTestCase
     public function testUserCanSeeMovieListPageTest()
     {
         $user = factory(User::class)->create();
-        $movie_1 = factory(Movie::class)->create();
-        $movie_2 = factory(Movie::class)->create();
+        $movie_1 = factory(Movie::class)->create(["release_date"=>"1997-02-17"]);
+        $movie_2 = factory(Movie::class)->create(["release_date"=>"1988-11-11"]);
+        $movie_3 = factory(Movie::class)->create(["release_date"=>"2018-02-11"]);
+        $movie_4 = factory(Movie::class)->create(["release_date"=>"1956-02-28"]);
+        $movie_5 = factory(Movie::class)->create(["release_date"=>"2016-06-30"]);
         $user->movies()->attach([
             $movie_1->id,
-            $movie_2->id
+            $movie_2->id,
+            $movie_3->id,
+            $movie_4->id,
+            $movie_5->id,
         ]);
 
-
-        $this->browse(function (Browser $browser) use ($user, $movie_1, $movie_2) {
+        $this->browse(function ($browser) use ($user, $movie_1, $movie_2, $movie_3, $movie_4, $movie_5) {
             $browser->visit("/users/{$user->id}")
-                ->assertSee($user->name)
-                ->assertSee($movie_1->title)
-                ->assertSee($movie_2->title);
+            ->assertSee($user->name)
+            ->assertSee($movie_1->title)
+            ->assertSee("Released in: " . date('Y', strtotime($movie_1->release_date)))
+            ->assertSee("{$movie_1->years_ago()} years ago")
+            ->assertSee($movie_2->title)
+            ->assertSee("Released in: " . date('Y', strtotime($movie_2->release_date)))
+            ->assertSee("{$movie_2->years_ago()} years ago")
+            ->assertSee($movie_3->title)
+            ->assertSee("Released in: " . date('Y', strtotime($movie_3->release_date)))
+            ->assertSee("{$movie_3->years_ago()} year ago")
+            ->assertSee($movie_3->title)
+            ->assertSee("Released in: " . date('Y', strtotime($movie_4->release_date)))
+            ->assertSee("{$movie_4->years_ago()} years ago")
+            ->assertSee($movie_5->title)
+            ->assertSee("Released in: " . date('Y', strtotime($movie_5->release_date)))
+            ->assertSee("{$movie_5->years_ago()} years ago");
         });
     }
 }
