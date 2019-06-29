dcf672da4947c6a028eb6423fde6de126a1f0018
abdullaqudrat <aqudrat630@gmail.com>
2019-06-28 20:13:54 -0600

    Adds login form test
    user cannot see login form if authenticated, redirected to user show page

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class LoginTest extends TestCase
 {
@@ -15,5 +16,11 @@ class LoginTest extends TestCase
         $response = $this->get('/login');
         $response->assertSuccessful();
         $response->assertViewIs('auth.login');
+    }
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->get('/login');
+        $response->assertRedirect("/users/{$user->id}");
     }
 }
